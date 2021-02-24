eda1939b8cd199c657c4bf47f27e51b323bd1303
hissinger <loje75@gmail.com>
2021-02-24 23:53:06 +0900

    add mosquitto.conf

--- node/index.js
+++ node/index.js
@@ -1,19 +1,19 @@
 const mqtt = require("mqtt");
 const client = mqtt.connect({
-  hostname: "",
-  port: 443,
-  protocol: "wss",
+  hostname: "193.122.99.85",
+  port: 80,
+  protocol: "ws",
 });
 
-client.on("connect", function () {
-  client.subscribe("presence", function (err) {
+client.on("connect", () => {
+  client.subscribe("presence", (err) => {
     if (!err) {
       client.publish("presence", "Hello mqtt");
     }
   });
 });
 
-client.on("message", function (topic, message) {
+client.on("message", (topic, message) => {
   // message is Buffer
   console.log(message.toString());
   client.end();
